5152b2be4ef55b51cc326a879985bf47a63f64dc
Chris Garaffa <chris@aghstrategies.com>
2025-01-03 12:00:22 -0500

    Civix upgrae

--- contributionstable.php
+++ contributionstable.php
@@ -90,37 +90,10 @@ function contributionstable_civicrm_install() {
 }
 
 /**
- * Implementation of hook_civicrm_uninstall
- */
-function contributionstable_civicrm_uninstall() {
-  return _contributionstable_civix_civicrm_uninstall();
-}
-
-/**
  * Implementation of hook_civicrm_enable
  */
 function contributionstable_civicrm_enable() {
   return _contributionstable_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- */
-function contributionstable_civicrm_disable() {
-  return _contributionstable_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- */
-function contributionstable_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _contributionstable_civix_civicrm_upgrade($op, $queue);
 }
 
 // /**
